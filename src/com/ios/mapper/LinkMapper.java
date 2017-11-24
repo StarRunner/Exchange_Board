89e7addbd598ff0e06d39613324f283c98dad6b4
yzcaodl <caoyizheng@yeah.net>
2017-11-24 15:39:20 +0800

    change package content - from 'liuyanzhao.blog' to 'ios'

---
+++ src/com/ios/mapper/LinkMapper.java
@@ -0,0 +1,17 @@
+package com.ios.mapper;
+
+import com.ios.entity.Link;
+
+public interface LinkMapper {
+    int deleteByPrimaryKey(Integer linkId);
+
+    int insert(Link record);
+
+    int insertSelective(Link record);
+
+    Link selectByPrimaryKey(Integer linkId);
+
+    int updateByPrimaryKeySelective(Link record);
+
+    int updateByPrimaryKey(Link record);
+}
